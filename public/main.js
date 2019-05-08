43fdcabacfc6ccf24ed1c879635068516b5a21c2
Amanda Leaders <alsleaders@gmail.com>
2019-05-08 17:21:13 -0400

    working on shuffle, push to html

--- public/main.js
+++ public/main.js
@@ -14,28 +14,45 @@ let face = [
   'Queen',
   'King'
 ]
-// let cardDeck = []
-// let newCard = 0
+let cardDeck = []
+// let shownDeck = []
 
-// const createDeck = () => {
-for (let i = 0; i < suit.length; i++) {
-  console.log(suit[i]) //get suit
-  for (let j = 0; j < face.length; j++) {
-    // get face
-    console.log(face[j] + ' of ' + suit[i]) // add suit to face
-    // newCard = face[j] + suit[i]
+const createDeck = () => {
+  for (let i = 0; i < suit.length; i++) {
+    console.log(suit[i]) // get suit
+    for (let j = 0; j < face.length; j++) {
+      // get face
+      console.log(face[j] + ' of ' + suit[i]) // add suit to face
+      let newCard = face[j] + suit[i]
+      // push card to array
+      cardDeck.push(newCard)
+      console.log('card pushed to deck')
+    }
   }
 }
-// }
-
-// push card to array
-// cardDeck.push(newCard)
 
 // push card to html
+
+// .slice shuffled card
+// .push shuffled card into new array  ----> shownDeck.push()
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+    document.querySelector('h1.hello-world').textContent =
+      'Pick a card, any card!'
+  }
+
+  const shuffle = () => {
+    for (let i = cardDeck.length; i > 1; i--) {
+      // select a random card we have not hit yet
+      const randomPosition = Math.floor(Math.random() * i)
+      // swap the current card with the random card
+      let holdingPattern = cardDeck[i]
+      cardDeck[i] = cardDeck[randomPosition]
+      cardDeck[randomPosition] = holdingPattern
+      console.log('cards shuffled')
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.deck').addEventListener('click', createDeck)
